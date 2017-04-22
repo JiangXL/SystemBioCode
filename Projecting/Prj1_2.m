34b2de144acf48a3f2ff872e003e9b6360554cd2
hey <heyh@mail.sustc.edu.cn>
2017-04-23 04:20:10 +0800

    Best minic parameter

--- Projecting/Prj1_2.m
+++ Projecting/Prj1_2.m
@@ -6,21 +6,19 @@
 %--------------------------------------------------------------------------
 close all;
 clear;
-Dox=[0.01:0.1:1 1:1:10 10:10:100]; % Dox浓度序列
-c=1e2; % c=a*pCMT  这是蛋白的表达，所以小(c越大，GFP越小,能调最大值)
-k1=9e6; % tetR-->tetR2,k1所以大, k-1小(如果k1很小，那GFP表达基本不受DOX调控)（与拐点有关？）
-k2=1e2; % 需要斟酌tetR2与Dox的反应。 
-theta=1e4 ; % Hill function, 是tetR2与 DNA binding sites的反应平衡常数。
-t=8.4e1;  % 时间长度,2天
+Dox=[0.01:0.01:1 1:0.1:10 10:1:100]; % Dox浓度序列
+c=8e-2; % c=a*pCMV 这是蛋白的表达，所以小(c越大，GFP越小,能调最大值)
+k1=5e3; % tetR-->tetR2,k1所以大, k-1小(如果k1很小，那GFP表达基本不受DOX调控)（与拐点有关？）
+k2=5e5; % 需要斟酌tetR2与Dox的反应。 
+theta=1e2 ; % Hill function, 是tetR2与 DNA binding sites的反应平衡常数。
+t=3e3;  % 时间长度,2天
 tetR=c*t; % t时间内tetR总生产量
-n=1;
+n=3;
+B=6e2;
 
 tetR2=k1*(tetR*tetR)./(k2*Dox) - (exp(-t*k2))./(k2*Dox);
 pCMV2=(theta.^n)./(theta.^n+tetR2.^n);
-GFP=pCMV2*t;
+GFP=(B*pCMV2+7)*t*0.005;
 title('Dose responese without feedback');
-loglog(Dox,pCMV2,'or');
-legend('GFP ');
-xlabel('Dox');
-ylabel('Concentration (M)');
-
+loglog(Dox,GFP,'or');
+ylim([1e1,1e4]);
