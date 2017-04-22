0a7234364a3c2aeab69e1430c285210c929d0c2d
Chris <11510511@mail.sustc.edu.cn>
2017-04-22 21:36:40 +0800

    数值解

--- Projecting/Prj1_2.m
+++ Projecting/Prj1_2.m
@@ -2,29 +2,23 @@
 % fuction of enzyme reaction
 %  | Version  | Contribution | Time | Comment|
 %  |   1.0     | œ”“…∑∏ «π‚Õ∑ | 2017.4.17| Initial verison from Prof.Huang
+%  |   1.1     | H.F.        | 2017.4.22 | 
 %--------------------------------------------------------------------------
 close all;
 clear;
+c=1.0;
+k1=1e3;
+k2=[ 0.001:0.1:1 1:1:10 10:10:100]; % K2*[Dox]
 
-a=1.0;
-k1=1e3; 			% units 1/(Ms)
-k2=1e3;
-theta=100 ;
-k5=1e2;
-pCMV1=10;
-tetR20=1;
-Dox=0.1;
-time=100;
-options=[];
+theta=100 ;        
+t=100;
 
-[t ,y]=ode45('nofeedback',[0 time],[pCMV1, tetR20],options,a,k1,k2,Dox);
+tetR=c*t;
+tetR2=k1.*(tetR*tetR)./k2- (exp(t*k2))/k2;
 
-tetR2=y(:,2);
-pCMV2=(theta)/(theta+tetR2);
-GFP=k5*pCMV2*time;
+pCMV2=(theta)./(theta+tetR2);
 
-
-figure(1);plot(log10(Dox),GFP,'.-r');
+figure(1);plot(log10(k2),pCMV2,'.-r');
 legend('GFP ');
 xlabel('Dox');
 ylabel('Concentration (M)');
