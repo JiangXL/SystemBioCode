136beea19824eb2bfe049eca2ddd8227de305e8f
hey <heyh@mail.sustc.edu.cn>
2017-04-23 19:51:30 +0800

    Edit by XJS

--- Projecting/Prj1_3.m
+++ Projecting/Prj1_3.m
@@ -1,20 +1,31 @@
 close all;
 clear;
 % 1=tetr 0  2=tetr2 0 3=dox   4=tetdn
-k1=5e3;
-k2=5e5;
-k_2=10^-3;
-n=3;
-theta=1e2;
+%k1=5e3;
+%k2=5e5;
+%k_2=10^-3;
+n=4;
+theta=0.44;
 options=[];
-
-for i =[1e-2:1e-2:1e-1 1e-1:1e-1:1 1:1:1e2]                                  
-[t ,y]=ode23('feedback',[0 100],[0 0 0],options,k1,k2,n,theta,i);
-tetr=y(end,1);
-tetr2=y(end,2);
-tetdn=y(end,3);
-m=n*(tetr+2*tetr2+2*tetdn);
-loglog(i,m,'*r');
-hold on;
+a=50;
+b=3.6;
+C=0.6;
+d=0.12;
+f=1.2;
+dox = [1e-2:1e-2:1 1:1e-1:10 10:1:100];
+m = zeros(1,length(dox));
+j=1;
+for i =dox
+[t ,y]=ode23('feedback',[0 100],[0 i 0],options,a,b,C,theta,n,d,f);
+m(j)=y(end,3);
+max(y(:,3))
+ j=j+1;
+%hold on
 end
+loglog(dox,m,'o-k');
+% plot([1:dox], m);
+xlabel('Doxycycline(ng/ml)');
+ylabel('Mean of mCherry');
+title('Dose responese with feedback');
+    grid on;
 
--- Projecting/feedback.m
+++ Projecting/feedback.m
@@ -2,14 +2,19 @@
 %--------------------------------------------------------------------------
 % fuction of enzyme reaction
 %  | Version  | Contribution | Time | Comment|
-%  |   1.0     | H.F.     | 2017.4.22| Initial version
+%  |   1.0     | wang xinyu     | 2017.4.22| Initial version
+%  |   1.1     |                | 4.23     |
 %--------------------------------------------------------------------------
 % [pCMV] = y(1), [tetR2] = y(2), [P] = y(3)
-function dmdt=f(t,y,flag,k1,k2,n,o,i)
-%1=tetr   2=tetr2    3=tetdn
-dmdt= [o^n/(o^n+y(2)^n);
-     (y(1))^2*k1-y(2)*i*k2;
-    -k2*y(3);];
+function dmdt=feedback(t,y,~,a,b,C,theta,n,d,f)
+% y(1)=tetR; y(2)=Dox; y(3)=GFP
+dmdt= [ a*my(y(1),theta,n)-b*y(1)*y(2)-d*y(1);
+      C-b*y(1)*y(2)-f*y(2);
+      a*my(y(1),theta,n)-y(3)*d];
+end
+
+function x=my(z,theta,n)
+            x=theta^n./(theta^n+z.^n);
 end
    % (y(1))^2*k1-y(2)*i*k2+y(3)*k_2;
     %-k2*y(3)+k_2*y(2)*i;];
