1e0511b0c0a0994eb24f65479cdf95aad495e3ad
H.F <11510511@mail.sustc.edu.cn>
2017-06-06 01:42:42 +0800

    Up figure of speed change

--- Projecting/Prj2_3.m
+++ Projecting/Prj2_3.m
@@ -1,10 +1,14 @@
 clear all;
 load channel_1.mat 
 % Single E.Coli ramdom walk
-X=tracks(13).tracksCoordAmpCG(1,1:8:end);
-Y=tracks(13).tracksCoordAmpCG(1,2:8:end);
+X=tracks(17).tracksCoordAmpCG(1,1:8:end);
+Y=tracks(17).tracksCoordAmpCG(1,2:8:end);
+X(isnan(X))=[];
+ylabel('Counter');
+Y(isnan(Y))=[];% delete NaN
+
 Y_end=[Y ,0];
-Y_start=[0, Y];
+Y_start=[0, Y];%take out NaN term
 trackY0=Y_end-Y_start;
 trackY1=trackY0(2:end-1);
 X_end=[X ,0];
@@ -14,22 +18,33 @@ trackX1=trackX0(2:end-1);
 
 figure(3);
 subplot(2,2,1);
-hist(trackX1,80);
+hist(trackX1/0.1,80);
 title('Single E.Coli random walk on X-axis');
 ylabel('Counter');
-xlabel('Move Distance/(mu m)');
+xlabel('Move Speed/(\mum/s)');
 
-
+%
 subplot(2,2,2);
-hist(trackY1,80);
+hist(trackY1/0.1,80);
 title('Single E.Coli random walk on Y-axis');
 ylabel('Counter');
-xlabel('Move Distance/(mu m)');
+xlabel('Move Speed/(\mum/s)');
 
+%Track of *E.Coli*
 subplot(2,2,3);
 plot(X,Y);
-%xlim();
-%ylim([0 848]);
+xlabel('X-axis/ \mum');
+ylabel('Y-axis/ \mum');
+title('Track of single bacteria');
+
+% speed(t)
+subplot(2,2,4);
+time=0.1:0.1:0.1*size(trackY1,2);
+v=sqrt((trackY1/0.1).^2+(trackX1/0.2).^1);
+plot(time,v);
+xlabel('Time/s');
+ylabel('Speed \mum/s');
+title('Speed during time');
 hold on;
 
 
--- Projecting/Prj2_3_2.m
+++ Projecting/Prj2_3_2.m
@@ -3,23 +3,25 @@ load channel_1.mat
 X=zeros(997,3183)+999999; Y=zeros(997,3183)+999999;
 cX=zeros(997,1); cY=zeros(997,1);
 cXX=zeros(997,1); cYY=zeros(997,1);
-%calculate the distance
+%Calculate the distance
+figure(5);
 for i=1:1:3183
 	time_0=tracks(i).seqOfEvents(1,1);
 	time_t=tracks(i).seqOfEvents(2,1);
-	X_temp=tracks(i).tracksCoordAmpCG(1:8:end);
-	Y_temp=tracks(i).tracksCoordAmpCG(2:8:end);
+	tracksnumber=size(tracks(i).seqOfEvents);
+	X_temp=tracks(i).tracksCoordAmpCG(1,1:8:end);
+	Y_temp=tracks(i).tracksCoordAmpCG(1,2:8:end);
 	for j=time_0+1:1:time_t-1  %frame start from 2:end-1
 		X_0=X_temp(j-time_0);
 		X_1=X_temp(j-time_0+1);
-		X(j-1,i)=X_1-X_0; %output to X. 
+		X(j-1,i)=X_1-X_0; %output to X
 		Y_0=Y_temp(j-time_0);
 		Y_1=Y_temp(j-time_0+1);
 		Y(j-1,i)=Y_1-Y_0;
 	end
 end
 
-%statical
+%Statical
 counter=zeros(997,1);
 for j=1:1:997
 	for i=1:1:3183
@@ -29,7 +31,6 @@ for j=1:1:997
 		else
 		cX(j)=cX(j)+X(j,i);		
 		cXX(j)=X(j,i)*X(j,i)+cXX(j);
-
 		end
 
 		if(Y(j,i)==999999||isnan(Y(j,i)))
@@ -53,29 +54,30 @@ for j=1:1:997
 	end
 end
 
-%figure
+% 4 Figures 
+figure(2);
 subplot(2,2,1);
 plot(0.1:0.1:99.7, cX);
-ylabel('c=X');
-xlabel('Time');
+ylabel('<Move Distance>');
+xlabel('Time/s');
 title('Random walk in X-axis');
 
 subplot(2,2,2)
 plot(0.1:0.1:99.7,cXX);
 title('Random walk in X-axis');
-ylabel('X^2');
-xlabel('Time');
+ylabel('<Move Distance^2>');
+xlabel('Time/s');
 
 subplot(2,2,3)
 plot(0.1:0.1:99.7,cY);
 title('Random walk in Y-axis');
-ylabel('cY');
-xlabel('Time');
+ylabel('<Move Distance>');
+xlabel('Time/s');
 
 subplot(2,2,4)
 plot(0.1:0.1:99.7,cYY);
 title('Random walk in Y-axis');
-ylabel('Y^2');
+ylabel('<Move Distance^2>');
 xlabel('Time/s');
 
 
