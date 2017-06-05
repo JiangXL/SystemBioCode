ffaa005e94e1cd3f92c0091c9cc56b1539046a3f
H.F <11510511@mail.sustc.edu.cn>
2017-06-05 18:28:45 +0800

    Remove populatoin stastica

--- Projecting/Prj2_3.m
+++ Projecting/Prj2_3.m
@@ -1,29 +1,35 @@
+clear all;
 load channel_1.mat 
-
-X=tracks(3).tracksCoordAmpCG(1:8:end);
-Y=tracks(3).tracksCoordAmpCG(2:8:end);
-
+% Single E.Coli ramdom walk
+X=tracks(13).tracksCoordAmpCG(1,1:8:end);
+Y=tracks(13).tracksCoordAmpCG(1,2:8:end);
 Y_end=[Y ,0];
 Y_start=[0, Y];
-
-trackY0=Y_end-Y_star;
+trackY0=Y_end-Y_start;
 trackY1=trackY0(2:end-1);
-figure(1);
-ylabel('counter');
-subplot(2,1,1);
-hist(trackY1,80);
-title('A E.Coli random walk in Y-axis');
-ylabel('Counter');
-xlabel('Move Distance/(mu m)');
-subplot(2,1,2);
-X_end=[X,0];
-X_start=[0,X];
-trackX0=X_end -X_start;
+X_end=[X ,0];
+X_start=[0, X];
+trackX0=X_end-X_start;
 trackX1=trackX0(2:end-1);
+
+figure(3);
+subplot(2,2,1);
 hist(trackX1,80);
-title('A E.Coli random walk in X-axis');
+title('Single E.Coli random walk on X-axis');
 ylabel('Counter');
 xlabel('Move Distance/(mu m)');
 
+
+subplot(2,2,2);
+hist(trackY1,80);
+title('Single E.Coli random walk on Y-axis');
+ylabel('Counter');
+xlabel('Move Distance/(mu m)');
+
+subplot(2,2,3);
+plot(X,Y);
+%xlim();
+%ylim([0 848]);
+hold on;
 
 
